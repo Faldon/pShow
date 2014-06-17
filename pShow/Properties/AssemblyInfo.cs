05d2b28bce668b060a205c61d095f9c8781a1ea5
Faldon <faldon@thesecretgamer.de>
2014-06-17 15:47:55 +0200

    Adjusted the assembly and file versions.

--- pShow/Properties/AssemblyInfo.cs
+++ pShow/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.518.1651")]
+[assembly: AssemblyVersion("1.2.5.0")]
+[assembly: AssemblyFileVersion("1.2.617.1546")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
